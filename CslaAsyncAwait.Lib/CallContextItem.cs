dc278f68fbe5eb1d6475ba8745616602db3e49a8
keithdv <keith.voels@aonbenfield.com>
2016-02-04 17:49:56 -0600

    Inheriting from MarshalByRefObject seems to solve all AppDomain issues in MSTEST

--- CslaAsyncAwait.Lib/CallContextItem.cs
+++ CslaAsyncAwait.Lib/CallContextItem.cs
@@ -9,32 +9,28 @@ using System.Threading.Tasks;
 namespace CslaAsyncAwait.Lib
 {
 
-    [Serializable]
+    /// <summary>
+    /// Wrap objects that get put into the CallContext to prevent Cross-Domain errors
+    /// CallContext was designed to permit communication across appdomains
+    /// when that happens some not func security stuff happens
+    /// Not an issue in WPF or IIS but breaks MSTEST itself
+    /// MarshalByRefObject seems to solve all of the issues
+    /// </summary>
     public class CallContextItem : MarshalByRefObject
     {
-        [NonSerialized]
-        private static Dictionary<Guid, WeakReference> itemStore = new Dictionary<Guid, WeakReference>();
-
         public CallContextItem(object item)
         {
-            itemStore.Add(UniqueIdentifier, new WeakReference(item));
+            this._item = item;
         }
 
+        private object _item;
 
         public object Item
         {
             get {
 
-                return itemStore[UniqueIdentifier].Target;
-
+                return _item;
             }
-        }
-
-        private readonly Guid _uniqueIdentifier = Guid.NewGuid();
-
-        public Guid UniqueIdentifier
-        {
-            get { return _uniqueIdentifier; }
         }
 
     }
